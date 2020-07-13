716901134a6672959a21e5d74f8255f87380043a
nirbhaynaruka <49834120+nirbhaynaruka@users.noreply.github.com>
2020-07-13 23:51:39 +0530

    updated link
    updated link

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ void(async() => {
         const browser = await puppeteer.launch({headless : false})
 
         const page = await browser.newPage()
-        await page.goto("https://academia.srmuniv.ac.in/accounts/signin?_sh=false&hideidp=true&portal=10002227248&client_portal=true&servicename=ZohoCreator&service_language=en&serviceurl=https%3A%2F%2Facademia.srmuniv.ac.in%2F")//website you want to access
+        await page.goto("https://academia.srmist.edu.in/accounts/signin?_sh=false&hideidp=true&portal=10002227248&client_portal=true&servicename=ZohoCreator&service_language=en&serviceurl=https%3A%2F%2Facademia.srmist.edu.in%2F")//website you want to access
 
 
         await page.type('#Email', 'Nirbhay_singh@srmuniv.edu.in');//input button if any on website
