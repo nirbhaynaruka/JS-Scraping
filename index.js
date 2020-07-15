e3d8429948447804d3cbcf86b688d1456f4fa462
nirbhaynaruka <49834120+nirbhaynaruka@users.noreply.github.com>
2020-07-15 11:18:08 +0530

    final commit
    final commit

--- index.js
+++ index.js
@@ -7,23 +7,21 @@ void(async() => {
        
         const browser = await puppeteer.launch({headless : false})
         //headless : true if you dont want browser to popup
-        
+
         const page = await browser.newPage()
         await page.goto("https://academia.srmist.edu.in/accounts/signin?_sh=false&hideidp=true&portal=10002227248&client_portal=true&servicename=ZohoCreator&service_language=en&serviceurl=https%3A%2F%2Facademia.srmist.edu.in%2F")//website you want to access
 
-        await page.type('#Email', 'Nirbhay_singh@srmuniv.edu.in');//input button if any on website
-        await page.type('#Password', 'nirbhay1999');
+        await page.type('#Email', '<your_id@srmist.edu.in>');//input button if any on website
+        await page.type('#Password', '<your password>');
         await page.click('input[type="button"]',{waitUntil: 'networkidle2'});
-
 
         await page.waitForNavigation();
         await page.screenshot({path: 'home.png'});
-       
+
         await page.goto("https://academia.srmuniv.ac.in/#View:My_Attendance")
         await page.waitForNavigation();
         await page.screenshot({path: 'Attendance.png'});
         await browser.close()
-
     }
     catch(error){
         console.log(error)
